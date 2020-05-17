dfac5879491a179a290c55f822e1be4648aa7745
swatiprajapati08 <prajapatiswati4@gmail.com>
2020-05-17 14:23:24 +0530

    added modification

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+@override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+   void getLocation() async{
+    Location location=Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+
+   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
